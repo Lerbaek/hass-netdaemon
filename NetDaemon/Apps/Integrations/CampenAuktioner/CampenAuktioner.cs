4f9a6cff1f378ff428f5972979105456a72aa711
Kristoffer Lerbæk Pedersen <github@lerbaek.dk>
2022-04-09 21:27:20 +0200

    Use new Auction project for CampenAuktioner

--- NetDaemon/Apps/Integrations/CampenAuktioner/CampenAuktioner.cs
+++ NetDaemon/Apps/Integrations/CampenAuktioner/CampenAuktioner.cs
@@ -1,19 +1,18 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using Lerbaek.NetDaemon.Common.Notifications;
+﻿using Lerbaek.Auctions.CampenAuktioner;
 
 namespace Lerbaek.NetDaemon.Apps.Integrations.CampenAuktioner;
 
-//[Focus]
+[Focus]
 [NetDaemonApp]
-public partial class CampenAuktioner
+public class CampenAuktioner
 {
   private readonly IFileSystem fileSystem;
   private readonly INotificationBuilder notificationBuilder;
   private readonly ILogger<CampenAuktioner> logger;
   private readonly IHaContext haContext;
-  private readonly HttpClient httpClient;
   private readonly IServices services;
   private readonly VarEntities varEntities;
+  private readonly CampenAuktionerSite campenAuktionerSite;
 
   public CampenAuktioner(IHaContext               ha,
                          INetDaemonScheduler      scheduler,
@@ -29,34 +28,18 @@ public partial class CampenAuktioner
     services = new Services(ha);
     varEntities = new VarEntities(ha);
     try
-    {
-      httpClient = httpClientFactory.CreateClient(nameof(CampenAuktioner));
-      CancellationTokenSource? cancellationTokenSource = null;
+    { 
+      var httpClient = httpClientFactory.CreateClient(nameof(CampenAuktioner));
+      campenAuktionerSite = new CampenAuktionerSite(logger, httpClient);
 
-      StartNewCheckForMatches("startup search");
-
-      void StartNewCheckForMatches(string context)
-      {
-        try
-        {
-          SetMatches("Searching ...");
-          cancellationTokenSource?.Cancel();
-          cancellationTokenSource = new CancellationTokenSource();
-          CheckForMatches(cancellationTokenSource.Token).Wait();
-        }
-        catch (Exception e)
-        {
-          logger.LogError($"An error occurred on {context}.");
-          logger.LogError("{Exception}", e.ToString());
-        }
-      }
+      CheckForMatches("startup search").Wait();
 
       varEntities.CampenWatchlist
         .StateChanges()
         .Where(s => s.Old?.State != s.New?.State)
-        .Subscribe(_ => StartNewCheckForMatches("updated watch list"));
+        .Subscribe(_ => CheckForMatches("updated watch list").Wait());
 
-      scheduler.RunEvery(FromHours(12), () => StartNewCheckForMatches("scheduled search"));
+      scheduler.RunEvery(FromHours(12), () => CheckForMatches("scheduled search").Wait());
     }
     catch
     {
@@ -65,38 +48,37 @@ public partial class CampenAuktioner
     }
   }
 
-  private async Task CheckForMatches(CancellationToken cancellationToken)
+  private async Task CheckForMatches(string context)
   {
-    var items = (await GetResults(cancellationToken))?.ToArray();
-    if (cancellationToken.IsCancellationRequested || items is null)
-      return;
-    var watchlist = varEntities.CampenWatchlist
-                               .State!
-                               .Split('\n')
-                               .Select(line => line.ToLowerInvariant()
-                                                   .Split(' ')
-                                                   .GroupBy(word => !word.StartsWith('-')));
-    if (cancellationToken.IsCancellationRequested)
-      return;
-    var matches = items.Where(i => i.Matches(watchlist)).ToArray();
-    if (cancellationToken.IsCancellationRequested)
-      return;
-
-    var markdown = string.Join($"{NewLine}{NewLine}---{NewLine}{NewLine}", matches.Select(m => m.Markdown));
-    if (cancellationToken.IsCancellationRequested)
-      return;
-
-    SetMatches(markdown, matches.Length);
-    if (cancellationToken.IsCancellationRequested)
-      return;
-    Notify(matches);
+    try
+    {
+      SetMatches("Searching ...");
+      await CheckForMatches();
+    }
+    catch (Exception e)
+    {
+      logger.LogError($"An error occurred on {context}.");
+      logger.LogError("{Exception}", e.ToString());
+    }
   }
 
-  private void Notify(Item[] matches)
+  private async Task CheckForMatches()
+  {
+    var watchlist = varEntities.CampenWatchlist
+      .State!
+      .Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+    var matches = await campenAuktionerSite.GetMatchesAsync(watchlist);
+    
+    Notify(matches.ToArray());
+  }
+
+  private void Notify(CampenAuktionerItem[] matches)
   {
     const string fileName = "alerted.txt";
     if (!fileSystem.File.Exists(fileName))
       fileSystem.File.Create(fileName).Dispose();
+    
     var alerted = fileSystem.File
                             .ReadAllText(fileName)
                             .Split(NewLine)
@@ -146,84 +128,4 @@ public partial class CampenAuktioner
         }
       });
   }
-
-  private async Task<IEnumerable<Item>?> GetResults(CancellationToken cancellationToken)
-  {
-    try
-    {
-      var page1 = await GetPage(1, cancellationToken);
-      if (cancellationToken.IsCancellationRequested || page1 is null)
-        return default;
-      var page1Results = new Dictionary<int, IEnumerable<Item>> {{ 1, ParseItems(page1) }};
-      var results = new ConcurrentDictionary<int, IEnumerable<Item>>(page1Results);
-      Parallel.For(2, page1["pageCount"]!.Value<int>() + 1,
-        i =>
-        {
-          var retries = 10;
-          while (retries --> 0)
-          {
-            try
-            {
-              var page = GetPage(i, cancellationToken).Result;
-              if (cancellationToken.IsCancellationRequested || page is null)
-                return;
-              var result = ParseItems(page);
-              results.TryAdd(i, result);
-              return;
-            }
-            catch
-            {
-              // ignored
-            }
-          }
-        });
-      return cancellationToken.IsCancellationRequested
-        ? default
-        : results.SelectMany(r => r.Value);
-    }
-    catch (Exception )
-    {
-      return default;
-    }
-  }
-
-  private static IEnumerable<Item> ParseItems(JObject page)
-  {
-    if (page == null)
-      throw new ArgumentNullException(nameof(page));
-    return page["results"]!.Select(r => new Item(r)).Where(item => item.Active);
-  }
-
-  private async Task<JObject?> GetPage(int page, CancellationToken cancellationToken)
-  {
-    var apiLink = GetApiLink(page);
-    try
-    {
-      await using var stream = await httpClient.GetStreamAsync(apiLink, cancellationToken);
-      if (cancellationToken.IsCancellationRequested)
-        return default;
-      using var streamReader = new StreamReader(stream);
-      using JsonReader jsonReader = new JsonTextReader(streamReader);
-      return await JObject.LoadAsync(jsonReader, cancellationToken);
-    }
-    catch (OperationCanceledException)
-    {
-      return default;
-    }
-    catch (AggregateException)
-    {
-      return default;
-    }
-    catch (HttpRequestException)
-    {
-      throw;
-    }
-    catch (Exception)
-    {
-      System.Diagnostics.Debugger.Break();
-      throw;
-    }
-  }
-
-  private static string GetApiLink(int page) => $"http://campenauktioner.hibid.com/api/v1/lot/list?pn={page}&ipp=100&isArchive=false";
 }
