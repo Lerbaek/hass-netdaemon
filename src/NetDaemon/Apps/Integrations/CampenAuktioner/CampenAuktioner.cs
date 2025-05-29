f8f03d0eaa3cc5fcde23b57e9291e919b6ecafd0
Kristoffer Lerbæk <github@lerbaek.dk>
2025-05-29 13:38:12 +0200

    Disable Campen Auktioner integration since the website seems to either block me or be closing

--- src/NetDaemon/Apps/Integrations/CampenAuktioner/CampenAuktioner.cs
+++ src/NetDaemon/Apps/Integrations/CampenAuktioner/CampenAuktioner.cs
@@ -3,7 +3,7 @@
 namespace Lerbaek.NetDaemon.Apps.Integrations.CampenAuktioner;
 
 //[Focus]
-[NetDaemonApp]
+//[NetDaemonApp]
 public class CampenAuktioner
 {
   private readonly IFileSystem _fileSystem;
